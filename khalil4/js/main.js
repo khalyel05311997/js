5f548f4cc3edfa45633e3ed4faa4bd03997910da
khalyel05311997 <41632457+khalyel05311997@users.noreply.github.com>
2018-07-26 14:36:32 +0200

    Add files via upload

---
+++ khalil4/js/main.js
@@ -0,0 +1,30 @@
+function affb() {
+
+
+document.querySelector("#p1").style.fontWeight = "900";
+}
+function affi() {
+document.getElementById("p1").style.fontStyle = "italic";
+
+
+}
+function affu() {
+
+document.querySelector("#p1").style.textDecoration = "underline";
+
+}
+
+function Size() {
+
+
+	var a=document.querySelector("#type2").value;
+	document.getElementById("p1").style.fontSize= a;
+}
+
+function style1(){
+
+var a=document.querySelector("#type1").value;
+document.getElementById("p1").style.fontFamily = a;
+
+		}
+
